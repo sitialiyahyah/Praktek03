ba7c640ff2a6c616c7026825d696c1c96d34bc15
sitialiyahyah <sitialiyah94.net@gmail.com>
2023-05-26 14:37:49 +0800

    menggunakan method get

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -8,5 +8,12 @@ public class SepedaAksi {
         s.setHarga(1000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
