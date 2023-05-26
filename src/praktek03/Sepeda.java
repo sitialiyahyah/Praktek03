39353b8d4bf5d3f38e954aa6d1dbaf4915e7a748
sitialiyahyah <sitialiyah94.net@gmail.com>
2023-05-26 13:42:46 +0800

    Membuat atribut

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package praktek03;
 
-/**
- *
- * @author asus
- */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
 }
